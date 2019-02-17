1bd11a693c04e463a34016cf95f31bd564b83806
Francis Karagu <fkaragu@gmail.com>
2019-02-17 14:48:46 +0300

    Modified the model file

--- app/models.py
+++ app/models.py
@@ -9,6 +9,49 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+class Movie:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,id,title,overview,poster,vote_average,vote_count):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
+        self.vote_average = vote_average
+        self.vote_count = vote_count
+
+class Review:
+
+    all_reviews = []
+
+    def __init__(self,movie_id,title,imageurl,review):
+        self.movie_id = movie_id
+        self.title = title
+        self.imageurl = imageurl
+        self.review = review
+
+
+    def save_review(self):
+        Review.all_reviews.append(self)
+
+
+    @classmethod
+    def clear_reviews(cls):
+        Review.all_reviews.clear()
+
+    @classmethod
+    def get_reviews(cls,id):
+
+        response = []
+
+        for review in cls.all_reviews:
+            if review.movie_id == id:
+                response.append(review)
+
+        return response
+
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
@@ -16,6 +59,10 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(40),unique = True, index=True)
     hash_pass = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True, index = True)
+
+    pitches = db.relationship('Pitch',backref='user',lazy='dynamic')
+    comments = db.relationship('Comment',backref='user',lazy='dynamic')
+
 
     @property
     def password(self):
@@ -33,3 +80,48 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pitch_content = db.Column(db.String())
+    pitch_category = db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitch(cls,id):
+        pitches = Pitch.query.filter_by(id=id).all()
+        return pitches
+
+    @classmethod
+    def get_all_pitches(cls):
+        pitches = Pitch.query.order_by('-id').all()
+        return pitches
+
+    @classmethod
+    def get_category(cls,cat):
+        category = Pitch.query.filter_by(pitch_category=cat).order_by('-id').all()
+        return category
+
+
+class Comment(db.Model):
+    __tablename__='comments'
+
+    id = db.Column(db.Integer,primary_key=True)
+    comment_content = db.Column(db.String())
+    pitch_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+        return comments
